dbff168a715bc8b2609602a878b446bbb5c6fa9f
Tinker Monkey <1573324+tinkermonkey@users.noreply.github.com>
2026-02-10 08:40:33 -0500

    Revert "[Fix] Add proper spacing in SVG path to prevent octal literal error"
    This reverts commit f79ca216efac944a2918d0c6aca1c1779a5ae9bc.

--- src/catalog/stories/4-chat/containers/ChatPanelContainer.stories.tsx
+++ src/catalog/stories/4-chat/containers/ChatPanelContainer.stories.tsx
@@ -59,7 +59,7 @@ export const ErrorState: Story = () => (
             <div className="flex items-start gap-3">
               <div className="flex-shrink-0">
                 <svg className="w-5 h-5 text-red-600 dark:text-red-400" fill="currentColor" viewBox="0 0 20 20">
-                  <path fillRule="evenodd" d="M10 18a8 8 0 1 0 0 -16 8 8 0 0 0 0 16zM8.707 7.293a1 1 0 0 0 -1.414 1.414L8.586 10l-1.293 1.293a1 1 0 1 0 1.414 1.414L10 11.414l1.293 1.293a1 1 0 0 0 1.414 -1.414L11.414 10l1.293 -1.293a1 1 0 0 0 -1.414 -1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
+                  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
                 </svg>
               </div>
               <div className="flex-1">
